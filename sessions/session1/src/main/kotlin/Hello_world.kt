67584987f61a3d057363eb2268effda84a1adcf9
SamBuseyne <sam.buseyne@student.ehb.be>
2021-10-05 11:37:22 +0200

    Hello_world_dynamic

--- sessions/session1/src/main/kotlin/Hello_world.kt
+++ sessions/session1/src/main/kotlin/Hello_world.kt
@@ -1,4 +1,13 @@
 fun main(){
-    println("Hello world")
+    val names: Array<String> = arrayOf("Dries", "Sam", "Matthias", "Guillaume")
+    val randomName = names.random()
+    greeting(randomName)
 }
+
+
+fun greeting(randomName: String):String{
+    println("Gooooodmorning ${randomName}")
+    return ""
+}
+
 
