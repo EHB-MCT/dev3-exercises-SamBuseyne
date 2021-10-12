f64bfd63d2669bb886438d62bdb2786adc146f77
SamBuseyne <sam.buseyne@student.ehb.be>
2021-10-12 11:05:32 +0200

    Challenge_1_created
    First challenge created for Game of Thrones theme mini-game.

--- sessions/session2/Adventure_game/src/main/kotlin/Main.kt
+++ sessions/session2/Adventure_game/src/main/kotlin/Main.kt
@@ -1,9 +1,24 @@
 fun main(){
-    println("Up and running")
     challengeOne()
 }
 
 fun challengeOne(){
     println("Winter is coming")
-    println("What house do you serve")
+    println("What house do you serve?")
+    val correctAnswers = setOf<String>("Lannister","Targaryen", "Stark")
+    val userAnswer = readLine()
+    if(correctAnswers.contains(userAnswer)){
+        succes()
+    }else{
+        gameOver()
+    }
+}
+
+fun succes(){
+    println("You are lucky!!")
+}
+
+fun gameOver(){
+    println("Wrong side, winter is coming!!!")
+    println("You are now a member of the Army of the dead")
 }
