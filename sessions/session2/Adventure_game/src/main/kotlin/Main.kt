25209a1f2f12df3551f77fae4391d83abea1d99e
SamBuseyne <sam.buseyne@student.ehb.be>
2021-10-12 12:44:33 +0200

    WIP_challenge_three
    Startup of choice a weapon game.

--- sessions/session2/Adventure_game/src/main/kotlin/Main.kt
+++ sessions/session2/Adventure_game/src/main/kotlin/Main.kt
@@ -19,14 +19,38 @@ fun challengeTwo(){
     val randomNumber = (1..6).random()
     //TODO: check strings
     if(randomNumber >= 4){
-        println("You have won the game")
+        challengeThree()
     }else{
         gameOver()
     }
 }
 
-
 fun gameOver(){
     println("Wrong, winter is coming!!!")
     println("You are now a member of the Army of the dead")
+}
+
+fun challengeThree(){
+    println("You face a horde of zombies, choice a weapon to fight them or flee? Choose wisely")
+    val options = setOf<String>("Axe","Wooden stock", "Sword", "Flee")
+    println("Options: Axe, Wooden stock, Sword or Flee")
+    val userOption = readLine()
+
+    if(userOption == "Axe"){
+        println("Good chose, you killed them all")
+
+    }
+    if(userOption == "Wooden stock"){
+        println("Oof that was a close one")
+    }
+
+    if(userOption == "Sword"){
+        println("They may rest in pieces")
+    }
+    if(userOption == "Flee"){
+        println("Run you bloody fool!!")
+    }
+
+
+
 }
