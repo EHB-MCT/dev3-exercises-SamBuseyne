55c8cdaf52992f7efd1884bfd860c6fc6d47b0f7
SamBuseyne <sam.buseyne@student.ehb.be>
2021-11-30 12:57:09 +0100

    Session_7_WIP
    Session_7 Part_2 not finished yet, stuck on creating new class with different settings.

--- sessions/session7/src/main/kotlin/Duolingo.kt
+++ sessions/session7/src/main/kotlin/Duolingo.kt
@@ -1,18 +1,30 @@
-public class Duolingo{
+public class Duolingo(var amountWords: Int, var language: String){
         val words = mutableListOf<Word>(
             Word(original = "Stutten", translated = "Boterhammen", language = "WestVlaams"),
             Word(original = "Forchette", translated = "Vork", language = "WestVlaams"),
             Word(original = "Boai", translated = "Trui", language = "WestVlaams"),
-            Word(original = "Veste", translated = "Trui", language = "WestVlaams"),
-            Word(original = "Rute", translated = "Raam", language = "WestVlaams")
+            Word(original = "Veste", translated = "Jas", language = "WestVlaams"),
+            Word(original = "Rute", translated = "Raam", language = "WestVlaams"),
+            Word(original = "Pattaten", translated = "Aardappelen", language = "WestVlaams"),
+            Word(original = "Rute", translated = "Raam", language = "WestVlaams"),
         )
 
-    fun play(){
-        words.random()
-        val currentWords = words
-        val userInput = readLine()
-        println(currentWords)
-        words.shuffle()
-        println(currentWords)
+    fun play(words: Int, lang:String){
+        val randomWords = words.shuffled().take(words).toMutableSet()
+        println(randomWords.count())
+
+        while(randomWords.isNotEmpty()){
+            val selectedWord = randomWords.random()
+            println("Zeg e ke ${selectedWord.original} int Nederlans")
+            val userInput = readLine()
+            if(userInput == selectedWord.translated){
+                println(randomWords.count())
+                println("Vree goed! Gemeugt verder doen door et volgende woord te vertalen")
+                randomWords.remove(selectedWord)
+            }else{
+                println("Das nie juste eh! Upnieuw")
+                println(randomWords.count())
+            }
+        }
     }
 }
--- sessions/session7/src/main/kotlin/Main.kt
+++ sessions/session7/src/main/kotlin/Main.kt
@@ -2,6 +2,12 @@ fun main(){
     duolingoApp()
 }
 fun duolingoApp(){
-    val app = Duolingo()
-    app.play()
+    val app = Duolingo(5,"WestVlaams")
+    println("Initializing game...")
+    println("...")
+    println("How many words do you want?")
+    val amountWords = readLine()
+    println("Language of the game?")
+    val language = readLine()
+    app.play(amountWords: Int, language: String)
 }
