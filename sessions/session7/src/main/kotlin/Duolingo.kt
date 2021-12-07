ad905b339e1efd6023c0eb50e9a894cabf3a1b67
SamBuseyne <sam.buseyne@student.ehb.be>
2021-12-07 09:37:31 +0100

    Session_7 WIP
    Finished 2A

--- sessions/session7/src/main/kotlin/Duolingo.kt
+++ sessions/session7/src/main/kotlin/Duolingo.kt
@@ -1,30 +1,46 @@
-public class Duolingo(var amountWords: Int, var language: String){
-        val words = mutableListOf<Word>(
-            Word(original = "Stutten", translated = "Boterhammen", language = "WestVlaams"),
-            Word(original = "Forchette", translated = "Vork", language = "WestVlaams"),
-            Word(original = "Boai", translated = "Trui", language = "WestVlaams"),
-            Word(original = "Veste", translated = "Jas", language = "WestVlaams"),
-            Word(original = "Rute", translated = "Raam", language = "WestVlaams"),
-            Word(original = "Pattaten", translated = "Aardappelen", language = "WestVlaams"),
-            Word(original = "Rute", translated = "Raam", language = "WestVlaams"),
-        )
+class Duolingo {
 
-    fun play(words: Int, lang:String){
-        val randomWords = words.shuffled().take(words).toMutableSet()
-        println(randomWords.count())
+    var roundSize: Int = 0
+    var language: String = ""
 
-        while(randomWords.isNotEmpty()){
-            val selectedWord = randomWords.random()
-            println("Zeg e ke ${selectedWord.original} int Nederlans")
-            val userInput = readLine()
-            if(userInput == selectedWord.translated){
-                println(randomWords.count())
-                println("Vree goed! Gemeugt verder doen door et volgende woord te vertalen")
-                randomWords.remove(selectedWord)
-            }else{
+    constructor(roundSize: Int = 5, language: String = "en") {
+        println("Hoevele woordn wildje per ronde?")
+        this.roundSize = readLine()!!.toInt()
+
+        println("Wukke taole kiesde? 'WestVlaams' of 'WestVlaams'")
+        this.language = readLine()!!
+    }
+
+    val words = mutableListOf<Word>(
+        Word(original = "Stutten", translated = "Boterhammen", language = "WestVlaams"),
+        Word(original = "Forchette", translated = "Vork", language = "WestVlaams"),
+        Word(original = "Boai", translated = "Trui", language = "WestVlaams"),
+        Word(original = "Veste", translated = "Jas", language = "WestVlaams"),
+        Word(original = "Rute", translated = "Raam", language = "WestVlaams"),
+        Word(original = "Pattaten", translated = "Aardappelen", language = "WestVlaams"),
+        Word(original = "Rute", translated = "Raam", language = "WestVlaams"),
+    )
+
+    fun play(){
+        var currentWords = words.filter { it.language == this.language }.toMutableSet()
+        currentWords = currentWords.shuffled().take(this.roundSize).toMutableSet()
+
+        while (currentWords.isNotEmpty()){
+            val selectedWord = currentWords.random()
+            println(currentWords.count())
+            println("Wuk is de vertaling van: ${selectedWord.original}")
+            val userAnswer = readLine()
+
+            if (userAnswer != selectedWord.translated) {
                 println("Das nie juste eh! Upnieuw")
-                println(randomWords.count())
+                currentWords.shuffled()
+            } else {
+                currentWords.remove(selectedWord)
+                println("Vree goed! Gemeugt verder doen door et volgende woord te vertalen")
             }
+
         }
+        println("Proficiat nu zieje nen echten pro, ad gaot over West-Vlaoms kootn")
+
     }
 }
--- sessions/session7/src/main/kotlin/Main.kt
+++ sessions/session7/src/main/kotlin/Main.kt
@@ -1,13 +1,4 @@
-fun main(){
-    duolingoApp()
-}
-fun duolingoApp(){
-    val app = Duolingo(5,"WestVlaams")
-    println("Initializing game...")
-    println("...")
-    println("How many words do you want?")
-    val amountWords = readLine()
-    println("Language of the game?")
-    val language = readLine()
-    app.play(amountWords: Int, language: String)
+fun main (){
+    val duolingo = Duolingo(6, "en")
+    duolingo.play()
 }
